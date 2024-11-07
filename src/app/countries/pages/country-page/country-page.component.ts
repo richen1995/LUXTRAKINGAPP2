1d7bed1688160e32a1973868df36321418eff9f4
richen1995 <richen1995@gmail.com>
2024-11-07 10:21:06 -0500

    Uso de ActivatedRoute para rediriguir a otras paguinas por el codigo dinamico de la urla uso se un observable dentro de otro switchMap, uso de operadores RXJS

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router} from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+    
+  ){}
 
+  ngOnInit(): void {
+    this.activatedRoute
+        .params
+        .pipe(
+          //switchMap => regresa un nuevo observable
+          switchMap(({id}) => this.countriesService.searchCountryByAlphaCode(id))
+ 
+        )
+        .subscribe((country) =>{
+            //console.log({country})
+            if(!country){
+              return this.router.navigateByUrl('');
+            }
+            console.log('TENEMOS UN PAIS');
+            return;
+        });
+    }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, catchError } from 'rxjs';
+import { Observable, of, catchError, map } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 //providedIn: Es para que el srv se exponga globalmente
@@ -10,6 +10,16 @@ export class CountriesService {
     private apiUrl: string = 'https://restcountries.com/v3.1'
     constructor(private http: HttpClient) { }
     
+    //Regresa un pais silo encuentra o null
+    searchCountryByAlphaCode(code: string):Observable<Country | null>{ 
+        const url = `${this.apiUrl}/alpha/${code}`
+        return this.http.get<Country[]>(url)
+            .pipe(
+                map(countries => countries.length > 0 ? countries[0]: null),
+                catchError( () =>  of(null))
+            );
+    }
+
     searchCapital(term: string):Observable<Country[]>{
         const url = `${this.apiUrl}/capital/${term}`
         return this.http.get<Country[]>(url)
@@ -21,6 +31,7 @@ export class CountriesService {
             );
     }//retorna un observable
 
+    //Busqueda por Pais
     searchCountry(country:string):Observable<Country[]>{
         const url = `${this.apiUrl}/name/${country}`
         return this.http.get<Country[]>(url)
@@ -29,6 +40,7 @@ export class CountriesService {
         );
     }
 
+    //Busqueda por Region
     searchRegion(region:string):Observable<Country[]>{
         const url = `${this.apiUrl}/region/${region}`
         return this.http.get<Country[]>(url)
